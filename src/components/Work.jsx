6e8aa6025079cabaca71b71bc4b0e3c71a5b5eb4
Anton <antongolovachukk@gmail.com>
2022-09-20 17:05:33 +0300

    bag fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,14 +10,14 @@ const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-8 pt-28">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
           <p className="py-6">Check out some of my recent work</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mt-3">
           <div
             style={{ backgroundImage: `url(${reactBg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
